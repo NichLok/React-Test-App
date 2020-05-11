d274417dd381b683b497b3a3d07ad2f04fb270e8
Nicholas <nicholaslok1996@gmail.com>
2020-05-11 20:35:36 +0800

    Rendering classes dynamically

--- nic-app/src/components/counter.jsx
+++ nic-app/src/components/counter.jsx
@@ -2,24 +2,23 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-  };
-
-  styles = {
-    fontSize: 20,
-    fontWeight: "bold",
+    count: 1,
   };
 
   render() {
     return (
       <div>
-        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">
-          {this.formatCount()}
-        </span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
