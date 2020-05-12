5f70031b75dd156939cb4fcb51373db9094718a3
Nicholas <nicholaslok1996@gmail.com>
2020-05-12 10:49:45 +0800

    Passing Event Arguments

--- nic-app/src/components/counter.jsx
+++ nic-app/src/components/counter.jsx
@@ -5,16 +5,21 @@ class Counter extends Component {
     count: 0,
   };
 
-  handleIncrement = () => {
+  handleIncrement = (product) => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
   };
+
+  // doHandleIncrement = () => {
+  //   this.handleIncrement({ id: 1 });
+  // };
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={(product) => this.handleIncrement(product)}
           className="btn btn-secondary btn-sm"
         >
           Increment
