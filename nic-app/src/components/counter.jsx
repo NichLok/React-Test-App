df0dfd23a91ad13befac04be47ca27e6f33f3929
Nicholas <nicholaslok1996@gmail.com>
2020-05-12 11:13:47 +0800

    Passing children

--- nic-app/src/components/counter.jsx
+++ nic-app/src/components/counter.jsx
@@ -10,10 +10,10 @@ class Counter extends Component {
   };
 
   render() {
-    //console.log("props", this.props);
-
+    console.log(this.props);
     return (
       <div>
+        {this.props.children}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           onClick={this.handleIncrement}
