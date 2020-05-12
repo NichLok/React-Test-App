7d3ae3c61952cf50023babf6c07bcc1b3b56edee
Nicholas <nicholaslok1996@gmail.com>
2020-05-12 12:49:53 +0800

    Single source of truth

--- nic-app/src/components/counters.jsx
+++ nic-app/src/components/counters.jsx
@@ -11,6 +11,14 @@ class Counters extends Component {
     ],
   };
 
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
   handleDelete = (counterID) => {
     const counters = this.state.counters.filter((c) => c.id !== counterID);
     this.setState({ counters });
@@ -19,6 +27,12 @@ class Counters extends Component {
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2"
+        >
+          Reset
+        </button>
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
